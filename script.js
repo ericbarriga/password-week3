cb9c40c3f7699971f15200105b396e639936240b
Eric Barriga <ericbarriga@MacBook-Pro.local>
2021-11-22 00:42:38 -0700

    did the generator hw again

--- script.js
+++ script.js
@@ -1,58 +1,92 @@
-const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
-const rand = Math.floor(Math.random() * letters.length);
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-const lenght = 8;
-
-function generatePassword(){
-  
- 
-  
-  return  getRandomNumber() + getRandomLower() + getRandomUpper() + getRandomSymbol() 
-  + length ; 
-}
-
-
-// Write password to the #password input
-function writePassword(){
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//dom elemets get elements from the html 
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
 
 
 
 
-
-
-
-
-/*const random ={
+//where to store all the random functions
+const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
-  symbols: getRandomSymbol,
-  length :  10,
-}*/
+  symbol: getRandomSymbol,
+};
 
 
 
-function getRandomLower(){
+// eventListener 
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasSymbol = symbolsEl.checked;
+  const hasNumber = numbersEl.checked;
+
+
+  resultEl.innerText = generatePassword(hasNumber, hasSymbol, hasUpper, hasLower, length);
+});
+
+
+
+// Generate password function 
+
+function generatePassword(lower, upper, number, symbol, length) {
+
+  let generatePassword = '';
+
+  const typesCount = lower + upper + number + symbol;
+
+
+  const typeArr = [{ lower }, { upper }, { number }, { symbol }].filter
+    (
+      item => Object.values(item)[0]
+    );
+
+
+  if (typesCount === 0) {
+    return '';
+  }
+  //loop 
+  for (let i = 0; i < length; i += typesCount) {
+    typeArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+
+      generatePassword += randomFunc[funcName]();
+    });
+  }
+  // final password 
+  const finalPassword = (generatePassword.slice(0, length));
+  return finalPassword;
+
+
+}
+
+
+// functions to get random letters and numbers 
+function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-function getRandomUpper(){
+
+function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function getRandomNumber(){
+function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-function getRandomSymbol(){
-  const symbols = '!@#$%^&*(){}[]=<>.,';
-  return symbols[Math.floor(Math.random()* symbols.length)];
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()_+=[]{}?/.,<>'
+  return symbols[Math.floor(Math.random() * symbols.length)]
 }
+
+
+
+
